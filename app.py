d27837992c29d447440901b50fef45cd9077e63b
Satyam Tiwari <satyam.12207441@lpu.in>
2024-08-14 19:51:05 +0530

    Add Dockerfile for containerization

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps!"
+     return "Hello, DevOps!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+     app.run(host='0.0.0.0', port=5000)
+
+# // const express = require('express');
+
+# // const app = express();
+
+# // app.get('/', (req, res) => {
+# //   res.send('Hello, DevOps!');
+# // });
+
+# // app.listen(5000, () => {
+# //   console.log('Server started on port 5000');
+# // });
